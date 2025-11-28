264ee777b5d325870fb86456b762f12f50ad6c69
richardjim <richardiyevhobu@gmail.com>
2025-11-28 09:24:53 +0100

    update origin

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000'], // frontend URL
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
